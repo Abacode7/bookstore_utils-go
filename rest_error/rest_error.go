2f1a9350a3fa129f173089864cafa38241568a91
Abraham Olamipo <aolamipo@teamapt.com>
2021-04-01 17:33:54 +0100

    change: modification made to rest error interface

--- rest_error/rest_error.go
+++ rest_error/rest_error.go
@@ -1,40 +1,67 @@
 package rest_error
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-type RestErr struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-	Error   string `json:"error"`
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
 }
 
-func NewBadRequestError(message string) *RestErr {
-	return &RestErr{Message: message,
-		Status: http.StatusBadRequest,
-		Error:  http.StatusText(http.StatusBadRequest),
+type restErr struct {
+	message string `json:"message"`
+	status  int    `json:"status"`
+	error   string `json:"error"`
+}
+
+func (re *restErr) Message() string {
+	return re.message
+}
+
+func (re *restErr) Status() int {
+	return re.status
+}
+
+func (re *restErr) Error() string {
+	return fmt.Sprintf(
+		"message: %s; status: %d; error; %s",
+		re.message,
+		re.status,
+		re.error,
+	)
+}
+
+func NewBadRequestError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status: http.StatusBadRequest,
+		error:  http.StatusText(http.StatusBadRequest),
 	}
 }
 
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   http.StatusText(http.StatusNotFound),
+func NewNotFoundError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   http.StatusText(http.StatusNotFound),
 	}
 }
 
-func NewInternalServerError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   http.StatusText(http.StatusInternalServerError),
+func NewInternalServerError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   http.StatusText(http.StatusInternalServerError),
 	}
 }
 
-func NewUnauthorizedError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status: http.StatusUnauthorized,
-		Error: http.StatusText(http.StatusUnauthorized),
+func NewUnauthorizedError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status: http.StatusUnauthorized,
+		error: http.StatusText(http.StatusUnauthorized),
 	}
 }
